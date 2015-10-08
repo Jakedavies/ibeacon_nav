74eccc97e2b5f9238131d3ce543fa00e46d53880
jakedavies <jgdavies24@gmail.com>
2015-10-08 15:39:42 -0700

    navigation layout added

--- app/src/main/java/info/jakedavies/innav/LocationListAdapter.java
+++ app/src/main/java/info/jakedavies/innav/LocationListAdapter.java
@@ -13,47 +13,21 @@ import android.widget.TextView;
  */
 public class LocationListAdapter extends ArrayAdapter<String> {
 
-    private String[] objects;
-    public LocationListAdapter(Context context,int resource, String[] objects) {
-        super(context, resource, objects);
-        this.objects = objects;
+    private String[] locations;
+    public LocationListAdapter(Context context,int resource, String[] locations) {
+        super(context, resource, locations);
+        this.locations = locations;
     }
-    public View getView(int position, View convertView, ViewGroup parent) {
-
-        // assign the view we are converting to a local variable
-        View v = convertView;
-
-        // first check to see if the view is null. if so, we have to inflate it.
-        // to inflate it basically means to render, or show, the view.
+    public View getView(int position, View v, ViewGroup parent) {
         if (v == null) {
             LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             v = inflater.inflate(R.layout.location_item, null);
         }
+        
+        String name = locations[position];
+        TextView lname = (TextView) v.findViewById(R.id.location_name);
+        lname.setText(name);
 
-		/*
-		 * Recall that the variable position is sent in as an argument to this method.
-		 * The variable simply refers to the position of the current object in the list. (The ArrayAdapter
-		 * iterates through the list we sent it)
-		 *
-		 * Therefore, i refers to the current Item object.
-		 */
-        String name = objects[position];
-
-        if (name != null) {
-
-            // This is how you obtain a reference to the TextViews.
-            // These TextViews are created in the XML files we defined.
-
-            TextView lname = (TextView) v.findViewById(R.id.location_name);
-
-            // check to see if each individual textview is null.
-            // if not, assign some text!
-            if (lname != null) {
-                lname.setText(name);
-            }
-        }
-
-        // the view must be returned to our activity
         return v;
     }
 }
--- app/src/main/java/info/jakedavies/innav/SectionListAdapter.java
+++ app/src/main/java/info/jakedavies/innav/SectionListAdapter.java
@@ -20,40 +20,16 @@ public class SectionListAdapter extends ArrayAdapter<String> {
     }
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        // assign the view we are converting to a local variable
         View v = convertView;
-
-        // first check to see if the view is null. if so, we have to inflate it.
-        // to inflate it basically means to render, or show, the view.
         if (v == null) {
             LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             v = inflater.inflate(R.layout.section_item, null);
         }
 
-		/*
-		 * Recall that the variable position is sent in as an argument to this method.
-		 * The variable simply refers to the position of the current object in the list. (The ArrayAdapter
-		 * iterates through the list we sent it)
-		 *
-		 * Therefore, i refers to the current Item object.
-		 */
         String name = objects[position];
+        TextView lname = (TextView) v.findViewById(R.id.section_name);
+        lname.setText(name);
 
-        if (name != null) {
-
-            // This is how you obtain a reference to the TextViews.
-            // These TextViews are created in the XML files we defined.
-
-            TextView lname = (TextView) v.findViewById(R.id.section_name);
-
-            // check to see if each individual textview is null.
-            // if not, assign some text!
-            if (lname != null) {
-                lname.setText(name);
-            }
-        }
-
-        // the view must be returned to our activity
         return v;
     }
 }
